ec38a1cc8e801bed6514873e1071dc97fce5c057
crotsos <cr409@cl.cam.ac.uk>
2013-02-26 17:26:22 +0000

    fixing example code

--- tests/controller.ml
+++ tests/controller.ml
@@ -23,17 +23,18 @@ type mac_switch = {
 
 type switch_state = {
 (*   mutable mac_cache: (mac_switch, OP.Port.t) Hashtbl.t; *)
-  mutable mac_cache: (OP.eaddr, OP.Port.t) Hashtbl.t; 
+  mac_cache: (OP.eaddr, OP.Port.t) Hashtbl.t;
+  ip_cache : (int32, OP.eaddr) Hashtbl.t;
   mutable dpid: OP.datapath_id list;
   mutable of_ctrl: OC.t list;
   req_count: int ref; 
 }
 
-let switch_data = 
+let sw_data () = 
   { mac_cache = Hashtbl.create 0; dpid = []; of_ctrl = []; 
-  req_count=(ref 0);} 
+  req_count=(ref 0);ip_cache = Hashtbl.create 0; } 
 
-let datapath_join_cb controller dpid evt =
+let datapath_join_cb switch_data controller dpid evt =
   let dp = 
     match evt with
       | OE.Datapath_join (c, _) -> c
@@ -42,7 +43,7 @@ let datapath_join_cb controller dpid evt =
   switch_data.dpid <- switch_data.dpid @ [dp];
   return (pp "+ datapath:0x%012Lx\n" dp)
 
-let packet_in_cb controller dpid evt =
+let packet_in_cb ?(handle_arp=true) name switch_data controller dpid evt =
   incr switch_data.req_count;
   let (in_port, buffer_id, data, dp) = 
     match evt with
@@ -52,51 +53,216 @@ let packet_in_cb controller dpid evt =
 
   (* Parse Ethernet header *)
   let m = OP.Match.raw_packet_to_match in_port data in 
+  let _ = printf "[openflow] received a packet from ip %s on ctrl %s\n%!" 
+          (Uri_IP.ipv4_to_string m.OP.Match.nw_src) name in 
 
   (* Store src mac address and incoming port *)
-  let ix = m.OP.Match.dl_src in
-  let _ = Hashtbl.replace switch_data.mac_cache ix in_port in
+  match (handle_arp, m.OP.Match.dl_type) with
+  | (_, 0x88cc) -> return ()
+  | (_, 0x0800)
+  | (true, 0x0806) -> begin 
+    let ix = m.OP.Match.dl_src in
+    let _ = Hashtbl.replace switch_data.mac_cache ix in_port in
  
-  (* check if I know the output port in order to define what type of message
-   * we need to send *)
- let ix = m.OP.Match.dl_dst in
-  if ( (ix = "\xff\xff\xff\xff\xff\xff")
-       || (not (Hashtbl.mem switch_data.mac_cache ix)) ) 
-  then (
-    let bs =
-      (OP.Packet_out.create ~buffer_id:buffer_id
-        ~actions:[ OP.(Flow.Output(Port.All, 2000))]
-        ~data:data ~in_port:in_port () ) in
-    let h = OP.Header.create OP.Header.PACKET_OUT 0 in
-    OC.send_data controller dpid (OP.Packet_out (h, bs))
-  ) else (
-    let out_port = (Hashtbl.find switch_data.mac_cache ix) in
-    let flags = OP.Flow_mod.({send_flow_rem=true; emerg=false; overlap=false;}) in 
-    lwt _ =
-      if (buffer_id = -1l) then
-        (* Need to send also the packet in cache the packet is not cached *)
-        let bs =
+    (* check if I know the output port in order to define what type of message
+     * we need to send *)
+    let ix = m.OP.Match.dl_dst in
+    if ( (ix = "\xff\xff\xff\xff\xff\xff")
+        || (not (Hashtbl.mem switch_data.mac_cache ix)) ) then (
+          let bs =
+            (OP.Packet_out.create ~buffer_id:buffer_id
+              ~actions:[ OP.(Flow.Output(Port.All, 2000))]
+              ~data:data ~in_port:in_port () ) in
+          let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+            OC.send_data controller dpid (OP.Packet_out (h, bs))
+    ) else (
+          let out_port = (Hashtbl.find switch_data.mac_cache ix) in
+          let flags = OP.Flow_mod.({send_flow_rem=true; emerg=false; overlap=false;}) in 
+          lwt _ =
+            if (buffer_id = -1l) then
+            (* Need to send also the packet in cache the packet is not cached *)
+              let bs =
                 OP.Packet_out.create
                    ~buffer_id:buffer_id
                    ~actions:[ OP.(Flow.Output(out_port, 2000))]
                    ~data:data ~in_port:in_port ()  in
-        let h = OP.Header.create OP.Header.PACKET_OUT 0 in
-          OC.send_data controller dpid (OP.Packet_out (h, bs))
-      else
-        return ()
-    in
-    let pkt =
+              let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+                OC.send_data controller dpid (OP.Packet_out (h, bs))
+            else
+              return ()
+          in
+          let pkt =
             (OP.Flow_mod.create m 0_L OP.Flow_mod.ADD ~hard_timeout:0
                  ~idle_timeout:0 ~buffer_id:(Int32.to_int buffer_id)  ~flags
                  [OP.Flow.Output(out_port, 2000)] ()) in
-        let h = OP.Header.create OP.Header.FLOW_MOD 0 in
-          OC.send_data controller dpid (OP.Flow_mod (h, pkt))
- )    
-    
-let init st = 
+          let h = OP.Header.create OP.Header.FLOW_MOD 0 in
+             OC.send_data controller dpid (OP.Flow_mod (h, pkt)) )    
+  end
+  | (_, _) -> 
+    let ix = m.OP.Match.dl_src in
+    let _ = Hashtbl.replace switch_data.mac_cache ix in_port in
+      return ()
+
+cstruct arp {
+  uint8_t dst[6];
+  uint8_t src[6];
+  uint16_t ethertype;
+  uint16_t htype;
+  uint16_t ptype;
+  uint8_t hlen;
+  uint8_t plen;
+  uint16_t op;
+  uint8_t sha[6];
+  uint32_t spa;
+  uint8_t tha[6];
+  uint32_t tpa
+} as big_endian
+
+let get_arp_reply mac dst_ip src_ip = 
+  let buf = OS.Io_page.to_cstruct (OS.Io_page.get ()) in 
+  let _ = set_arp_dst mac 0 buf in 
+  let _ = set_arp_src "\xfe\xff\xff\xff\xff\xff" 0 buf in 
+  let _ = set_arp_ethertype buf 0x0806 in  (* ARP *)
+  let _ = set_arp_htype buf 1 in
+  let _ = set_arp_ptype buf 0x0800 in  (* IPv4 *)
+  let _ = set_arp_hlen buf 6 in  (* ethernet mac size *)
+  let _ = set_arp_plen buf 4 in  (* ipv4 size *)
+  let _ = set_arp_op buf 2 in 
+  let _ = set_arp_sha "\xfe\xff\xff\xff\xff\xff" 0 buf in 
+  let _ = set_arp_spa buf src_ip in 
+  let _ = set_arp_tha mac 0 buf in 
+  let _ = set_arp_tpa buf dst_ip in 
+  (* Resize buffer to sizeof arp packet *)
+    Cstruct.sub buf 0 sizeof_arp
+
+
+let test_packet_out controller dpid actions data in_port = 
+  let buffer_id = -1l in 
+  let bs = OP.Packet_out.create ~buffer_id ~actions ~data ~in_port () in
+  let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+(*  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in
+  lwt _ = OC.send_data controller dpid (OP.Packet_out (h, bs)) in*)
+    return ()
+
+let router_packet_in_cb name switch_data controller dpid evt =
+  incr switch_data.req_count;
+  let (in_port, buffer_id, data, dp) = 
+    match evt with
+      | OE.Packet_in (inp, _, buf, dat, dp) -> (inp, buf, dat, dp)
+      | _ -> invalid_arg "bogus datapath_join event match!"
+  in
+
+  (* Parse Ethernet header *)
+  let m = OP.Match.raw_packet_to_match in_port data in 
+  let _ = printf "[openflow] received a packet %s - %s %x on ctrl %s\n%!" 
+          (Uri_IP.ipv4_to_string m.OP.Match.nw_src) 
+          (Uri_IP.ipv4_to_string m.OP.Match.nw_dst) 
+          m.OP.Match.dl_type name in 
+  
+  let ix = m.OP.Match.dl_src in
+  let _ = Hashtbl.replace switch_data.mac_cache ix in_port in
+
+  let _ = 
+    if ((m.OP.Match.dl_type = 0x0806) || (m.OP.Match.dl_type = 0x0800)) then
+      Hashtbl.replace switch_data.ip_cache
+        m.OP.Match.nw_src m.OP.Match.dl_src
+  in
+
+  (* Store src mac address and incoming port *)
+  match (m.OP.Match.dl_type) with
+  | (0x88cc) -> return ()
+  | 0x0806 when (Int32.logand m.OP.Match.nw_dst 0xffl = 1l) -> 
+      let ix = m.OP.Match.dl_src in
+      let data = get_arp_reply ix m.OP.Match.nw_src m.OP.Match.nw_dst in 
+      let bs =
+        (OP.Packet_out.create ~buffer_id:(-1l)
+              ~actions:[ OP.(Flow.Output(m.OP.Match.in_port, 2000))]
+              ~data ~in_port:(OP.Port.All) () ) in
+      let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+        OC.send_data controller dpid (OP.Packet_out (h, bs))
+  | 0x0800  
+      when ((Int32.logand m.OP.Match.nw_dst 0xffffff00l) <>
+            (Int32.logand m.OP.Match.nw_src 0xffffff00l)) -> begin
+     try_lwt
+     let ix = Hashtbl.find switch_data.ip_cache m.OP.Match.nw_dst in 
+     let out_port = Hashtbl.find switch_data.mac_cache ix in
+     let flags=OP.Flow_mod.({send_flow_rem=true;emerg=false;overlap=false;}) in
+     let actions = [OP.(Flow.Set_dl_dst(ix));
+                    OP.(Flow.Set_dl_src("\xfe\xff\xff\xff\xff\xff"));
+                    OP.(Flow.Output(out_port, 2000))] in 
+     lwt _ =
+       if (buffer_id = -1l) then
+            (* Need to send also the packet in cache the packet is not cached *)
+         let bs =
+           OP.Packet_out.create ~buffer_id ~actions ~data ~in_port ()  in
+          let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+            OC.send_data controller dpid (OP.Packet_out (h, bs))
+        else return () in
+    lwt _ =  test_packet_out controller dpid actions data in_port in 
+    let pkt =
+        OP.Flow_mod.create m 0_L OP.Flow_mod.ADD ~hard_timeout:0
+           ~idle_timeout:0 ~buffer_id:(Int32.to_int buffer_id)  ~flags
+           actions () in
+     let h = OP.Header.create OP.Header.FLOW_MOD 0 in
+      OC.send_data controller dpid (OP.Flow_mod (h, pkt)) 
+     with Not_found -> return ()
+  end
+(*  | 0x0800 
+  | 0x0806 -> begin 
+    (* check if I know the output port in order to define what type of message
+     * we need to send *)
+    let ix = m.OP.Match.dl_dst in
+    if ( (ix = "\xff\xff\xff\xff\xff\xff")
+        || (not (Hashtbl.mem switch_data.mac_cache ix)) ) then (
+          let bs =
+            (OP.Packet_out.create ~buffer_id:buffer_id
+              ~actions:[ OP.(Flow.Output(Port.All, 2000))]
+              ~data:data ~in_port:in_port () ) in
+          let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+            OC.send_data controller dpid (OP.Packet_out (h, bs))
+    ) else (
+          let out_port = (Hashtbl.find switch_data.mac_cache ix) in
+          let flags = OP.Flow_mod.({send_flow_rem=true; emerg=false; overlap=false;}) in 
+          lwt _ =
+            if (buffer_id = -1l) then
+            (* Need to send also the packet in cache the packet is not cached *)
+              let bs =
+                OP.Packet_out.create
+                   ~buffer_id:buffer_id
+                   ~actions:[ OP.(Flow.Output(out_port, 2000))]
+                   ~data:data ~in_port:in_port ()  in
+              let h = OP.Header.create OP.Header.PACKET_OUT 0 in
+                OC.send_data controller dpid (OP.Packet_out (h, bs))
+            else
+              return ()
+          in
+          let pkt =
+            (OP.Flow_mod.create m 0_L OP.Flow_mod.ADD ~hard_timeout:0
+                 ~idle_timeout:0 ~buffer_id:(Int32.to_int buffer_id)  ~flags
+                 [OP.Flow.Output(out_port, 2000)] ()) in
+          let h = OP.Header.create OP.Header.FLOW_MOD 0 in
+             OC.send_data controller dpid (OP.Flow_mod (h, pkt)) )    
+  end *)
+   | (_) -> 
+       let _ = printf "XXXXXX No idea what this is\n%!" in 
+       return ()
+     
+let init ?(handle_arp=true) name switch_data st = 
   if (not (List.mem st switch_data.of_ctrl)) then
     switch_data.of_ctrl <- (([st] @ switch_data.of_ctrl));
-  OC.register_cb st OE.DATAPATH_JOIN datapath_join_cb;
-  OC.register_cb st OE.PACKET_IN packet_in_cb
+  OC.register_cb st OE.DATAPATH_JOIN (datapath_join_cb switch_data);
+  OC.register_cb st OE.PACKET_IN (packet_in_cb ~handle_arp name switch_data)
 
-
+let init_router name switch_data st =
+  let _ = 
+    if (not (List.mem st switch_data.of_ctrl)) then
+      switch_data.of_ctrl <- (([st] @ switch_data.of_ctrl))
+  in 
+  let _ = OC.register_cb st OE.DATAPATH_JOIN (datapath_join_cb switch_data) in
+  OC.register_cb st OE.PACKET_IN 
+    (router_packet_in_cb name switch_data)
--- tests/rdsf_test.ml
+++ tests/rdsf_test.ml
@@ -22,6 +22,9 @@ open Printf
 
 module OP = Openflow.Ofpacket
 module OC = Openflow.Ofcontroller
+module OSK = Openflow.Ofsocket
+module OSW = Openflow.Ofswitch
+module OF = Openflow.Flowvisor
 module OE = OC.Event
 
 let port = 55555
@@ -29,52 +32,81 @@ let use_dhcp = false
 
 let pp = Printf.printf
 
+let log_dev id ip = 
+  let Some(node_name) = Lwt.get OS.Topology.node_name in
+  let msg = Rpc.Dict [
+    ("name", (Rpc.String node_name));
+    ("dev_id", (Rpc.String id));
+    ("ip", (Rpc.String ip));] in
+      OS.Console.broadcast "node_dev" (Jsonrpc.to_string msg)
+ 
+
 (*********************************************************
  * Host configuration code and traffic generation code 
  *********************************************************)
 let ip gid nid = 
-  Nettypes.(
-    (ipv4_addr_of_tuple (10l,0l,(Int32.of_int gid),(Int32.of_int nid)),
-    ipv4_addr_of_tuple (255l,255l,255l,0l),[ ])) 
+  (ipv4_addr_of_tuple (10l,0l,(Int32.of_int gid),(Int32.of_int nid)),
+    ipv4_addr_of_tuple (255l,255l, 0l,0l),
+    [(ipv4_addr_of_tuple (10l, 0l, (Int32.of_int gid), 1l)) ]) 
       
 (* Code to run on the end node *)
 let host_inner gid hid () =
   try_lwt 
     Manager.create (fun mgr interface id ->
-      lwt _ = OS.Time.sleep 1. in 
+      lwt _ = Time.sleep 0.5 in 
       lwt _ = Manager.configure interface (`IPv4 (ip gid hid)) in
-      let _ = printf "[host%d] server setting up ip 10.0.%d.%d\n%!" 
-                    hid gid hid  in  
+      let _ = log_dev id (sprintf "10.0.%d.%d" gid hid) in 
         match hid with
-        | 1 ->
+        | 2 ->
 (*            Datagram.UDPv4.recv mgr (None, port) echo_udp *)
-            Net.Channel.listen mgr (`TCPv4 ((None, port), Client.echo ))
-(*            Client.pttcp_server mgr port 30 *)
-        | 2 -> 
-            let dst_ip = 
-              Nettypes.ipv4_addr_of_tuple (10l,0l,(Int32.of_int gid),1l) in  
-            let _ = Printf.printf "[host%d] %f: connecting client \n%!"
-                    hid (Clock.time ()) in 
+(*            Net.Channel.listen mgr (`TCPv4 ((None, port), Client.echo ))*)
+            Client.pttcp_server mgr port 5
+        | 3 ->
+            let dst_gid = (gid + 1) mod 2 in 
+            let rem_dst_ip = 
+              ipv4_addr_of_tuple (10l,0l,(Int32.of_int dst_gid),2l) in  
+            let loc_dst_ip = 
+              ipv4_addr_of_tuple (10l,0l,(Int32.of_int gid),2l) in  
 (*            echo_client_udp mgr (dst_ip,port) *)
-            Net.Channel.connect mgr 
-                  (`TCPv4 (None, (dst_ip, port), Client.echo_client ))
-(*            Client.pttcp_client mgr dst_ip port 30 100000l *)
+(*            Net.Channel.connect mgr 
+                  (`TCPv4 (None, (dst_ip, port), Client.echo_client ))*)
+(*            Client.pttcp_client mgr rem_dst_ip port 5 100000l<&> *)
+            Client.pttcp_client mgr loc_dst_ip port 5 100000l 
         | _ -> return (printf "Invalid node_id %d\n%!" hid)
         )
     with e -> return (Printf.eprintf "Error: %s" (Printexc.to_string e))
+
 (*
  * controller code 
  * *)
-let controller_inner () = 
-  try_lwt 
+let router_inner () = 
+  try_lwt
+  let switch_data = Controller.sw_data () in 
     Manager.create (fun mgr interface id ->
       let ip = 
-        Nettypes.(
-          (ipv4_addr_of_tuple (192l,168l,0l,2l),
-          ipv4_addr_of_tuple (255l,255l,255l,0l), [])) in  
-     lwt _ = Manager.configure interface (`IPv4 ip) in
-     let dst = (None, 6633) in 
-        OC.listen mgr dst Controller.init 
+          (ipv4_addr_of_tuple (192l,168l,2l,2l),
+          ipv4_addr_of_tuple (255l,255l,255l,0l), []) in  
+      lwt _ = Manager.configure interface (`IPv4 ip) in
+      let _ = log_dev id "192.168.3.2" in 
+        OC.listen mgr (None, 6633) 
+        (Controller.init_router "router" switch_data )
+      )
+  with exn -> 
+    return (printf "%f: controller error: %s\n%!" (Clock.time ()) 
+            (Printexc.to_string exn))
+
+let controller_inner ctrl_id () = 
+  try_lwt
+    let switch_data = Controller.sw_data () in 
+    Manager.create (fun mgr interface id ->
+      let ip = 
+          (ipv4_addr_of_tuple (192l,168l,(Int32.of_int ctrl_id),2l),
+          ipv4_addr_of_tuple (255l,255l,255l,0l), []) in  
+      lwt _ = Manager.configure interface (`IPv4 ip) in
+      let name = sprintf "controller%d" ctrl_id in 
+      let _ = log_dev id (sprintf "192.168.%d.2" ctrl_id) in 
+        OC.listen mgr (None, 6633) 
+        (Controller.init ~handle_arp:false name switch_data)
       )
   with exn -> 
     return (printf "%f: controller error: %s\n%!" (Clock.time ()) 
@@ -83,76 +115,83 @@ let controller_inner () =
 (*
  * flow visor node
  * *)
-let init_slice mgr st =
-  let _ = printf "[flowvisor] connecting to controller\n%!" in 
-  let dst_ip = ((ipv4_addr_of_tuple (192l,168l,0l,2l)), 6633)  in
-  lwt _ = Openflow.Flowvisor.add_slice mgr st
-            (OP.Match.create_flow_match (OP.Wildcards.full_wildcard ()) () ) 
-            dst_ip 0x10L in 
-  return ()
+let init_slice mgr st id =
+  let dpid = Int64.add 0x10L (Int64.of_string id) in 
+  let subnet = Uri_IP.string_to_ipv4 (sprintf "10.0.%s.1" id) in  
+  let dst_ip = ((ipv4_addr_of_tuple (192l,168l,(Int32.of_string id),2l)), 6633)  in
+  match id with 
+  | "0" | "1" -> 
+      OF.add_slice mgr st 
+      (OP.Match.create_match ~nw_src:(Some subnet) ~nw_src_len:24
+        ~nw_dst:(Some subnet) ~nw_dst_len:24 ()) dst_ip dpid
+  | "2" ->      
+      OF.add_slice mgr st (OP.Match.create_match ()) dst_ip dpid
+ 
 
+let configure_ctrl_intf mgr sw intf id  = 
+  let ip = 
+      (ipv4_addr_of_tuple (192l, 168l, (Int32.of_string id), 1l),
+       ipv4_addr_of_tuple (255l,255l,255l,0l), []) in  
+  lwt _ = Manager.configure intf (`IPv4 ip) in
+  let _ = log_dev id (sprintf "192.168.%s.1" id) in 
+    init_slice mgr sw id
+
+let configure_sw_intf mgr sw intf id switch_id = 
+  let ip = 
+    (ipv4_addr_of_tuple (172l,16l,(Int32.of_int !switch_id),1l),
+      ipv4_addr_of_tuple (255l,255l,255l,0l), []) in
+  let _ = switch_id := !switch_id + 1 in 
+  let _ = log_dev id (sprintf "172.16.%d.1" !switch_id) in 
+  lwt _ = Manager.configure intf (`IPv4 ip) in
+    OF.listen sw mgr (None, 6633)
+ 
 let flowvisor_inner () = 
-  let sw = Openflow.Flowvisor.create_flowvisor () in
   try_lwt
-    let t, u = Lwt.task () in 
+    let sw = Openflow.Flowvisor.create_flowvisor () in
+    let switch_id = ref 1 in 
     Manager.create 
-      (fun mgr interface id ->
-           match id with
-             | "0" -> 
-                 let ip = 
-                   Nettypes.(
-                     (ipv4_addr_of_tuple (192l, 168l, 0l, 1l),
-                      ipv4_addr_of_tuple (255l,255l,255l,0l), [])) in  
-                      lwt _ = Manager.configure interface (`IPv4 ip) in
-                 let _ = printf "XXXXXXX found intf 0\n%!" in
-                 lwt _ = init_slice mgr sw in 
-                  return ()
-             | _ -> 
-                 let ip = 
-                   Nettypes.(
-                     (ipv4_addr_of_tuple (10l,0l,(Int32.of_string id),1l),
-                      ipv4_addr_of_tuple (255l,255l,255l,0l), [])) in
-                 let _ = printf "[flowvisor] setting ip on id %s 10.0.%s.1\n%!" 
-                        id id in 
-                 lwt _ = Manager.configure interface (`IPv4 ip) in
-                 lwt _ = Openflow.Flowvisor.listen sw mgr (None, 6633) in
-                   t)
-  with e ->
-    Printf.eprintf "Error: %s" (Printexc.to_string e); 
-    return ()
+      (fun mgr intf id -> 
+        match id with
+        | "0" | "1" | "2" -> configure_ctrl_intf mgr sw intf id
+        | _ -> configure_sw_intf mgr sw intf id switch_id)
+  with e -> return (Printf.eprintf "Error: %s" (Printexc.to_string e))
 
 (****************************************************************
  * OpenFlow Switch configuration 
  *****************************************************************)
 let switch_inner switch_id () = 
-  let sw = Openflow.Ofswitch.create_switch (Int64.of_int switch_id) in
+  let switch_data = Controller.sw_data () in 
+  let sw = OSW.create_switch (Int64.of_int switch_id) in
+  let flv = OF.create_flowvisor () in
+  let ctrl = OC.init_controller () in 
+  let (ctrl_ch1, ctrl_ch2) = OSK.init_local_conn_state () in
+  let (sw_ch1, sw_ch2) = OSK.init_local_conn_state () in
+
+  (* start local controller and local flowvisor threads in run mode *)
+  let name = sprintf "switch%d" switch_id in 
+  let _ = ignore_result (OF.local_listen flv sw_ch1) in 
+  let _ = ignore_result 
+          (OC.local_connect ctrl ctrl_ch1 (Controller.init name switch_data)) in
+
+  (* forward arp traffic to local controller *)
+  let dpid = Int64.add 0x20L (Int64.of_int switch_id) in
+  lwt _ = OF.add_local_slice flv 
+            (OP.Match.create_match ~dl_type:(Some 0x0806) ()) 
+            ctrl_ch2 dpid in   
+  
   try_lwt 
     Manager.create 
-    (fun mgr interface id ->
-       match (id) with 
-         | "0" ->
-             let _ = printf "[switch%d] setting device %s ip 10.0.%d.2\n%!" 
-                      switch_id id switch_id in 
-             let ip = 
-               Nettypes.(
-                 (ipv4_addr_of_tuple (10l,0l,(Int32.of_int switch_id),2l),
-                  ipv4_addr_of_tuple (255l,255l,255l,0l), [])) in  
-               lwt _ = Manager.configure interface (`IPv4 ip) in
-               let dst_ip = ipv4_addr_of_tuple (10l,0l,(Int32.of_int switch_id),1l) in
-               lwt _ = (Openflow.Ofswitch.connect sw mgr (dst_ip, 6633)) in 
-                return ()
-         | _ -> 
-             let Some(node_name) = Lwt.get OS.Topology.node_name in
-             let msg = Rpc.Dict [
-               ("name", (Rpc.String node_name));
-               ("dev_id", (Rpc.String id));
-               ("ip", (Rpc.String ""));] in
-             let _ = printf "XXXXXXXXX %s\n%!" (Jsonrpc.to_string msg) in 
-             let _ = OS.Console.broadcast "node_dev" (Jsonrpc.to_string msg) in 
-               Openflow.Ofswitch.add_port mgr sw id
+    (fun mgr intf -> function
+      | "0" ->
+          let ip = 
+            (ipv4_addr_of_tuple (172l,16l,(Int32.of_int switch_id),2l),
+            ipv4_addr_of_tuple (255l,255l,255l,0l), []) in  
+          let _ = log_dev "0" (sprintf "172.16.%d.2" switch_id) in 
+          lwt _ = Manager.configure intf (`IPv4 ip) in
+          let dst_ip = ipv4_addr_of_tuple (172l,16l,(Int32.of_int switch_id),1l) in
+          lwt _ = OF.add_slice mgr flv (OP.Match.create_match ())
+                    (dst_ip, 6633) (Int64.add 0x30L (Int64.of_int switch_id)) in
+            OSW.local_connect sw mgr sw_ch2
+      | id ->  let _ = log_dev id "" in OSW.add_port mgr sw id
     )
-  with e ->
-    Printf.eprintf "Error: %s" (Printexc.to_string e); 
-    return ()
-
-
+  with e -> return (Printf.eprintf "Error: %s" (Printexc.to_string e))
