da33041dfdd39d84908941af642d3e488da5bfe9
crotsos <cr409@cl.cam.ac.uk>
2013-11-10 22:29:04 +0000

    reverting to latest build system

--- src/ns3.ml
+++ src/ns3.ml
@@ -1,3 +1,19 @@
+(*
+ * Copyright (c) 2012 Charalampos Rotsos <cr409@cl.cam.ac.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
 open Printf
 open Lwt
 open Topo
@@ -46,13 +62,27 @@ let generate_scenario sc =
      let build_simulation module_name = *)
   let _ = Unix.system (sprintf "rm %s.native" module_name) in 
   let _ = Unix.system "ocamlbuild -clean" in 
-  let _ = Unix.system (sprintf "ocamlbuild topo_%s.nobj.o" module_name) in 
+  let path = "/home/cr409/.opam/4.00.1+mirage-ns3-direct/" in 
+   (* TODO make he bin patgh dynamic *)
+  let _ = Util.command "PATH=%s/bin/:$PATH %s/bin/ocamlbuild topo_%s.nobj.o"
+      path path module_name in
+  let obj = sprintf "_build/topo_%s.nobj.o" module_name in 
+  if Sys.file_exists obj then begin
+    let path = Util.read_command "%s/bin/ocamlfind printconf path" path in
+    let lib = Util.strip path ^ "/mirage" in
+
+    Util.command "g++ _build/topo_%s.nobj.o %s/libns3run.a %s/../ocaml/libunix.a  %s/../ocaml/libasmrun.a %s/../cstruct/libcstruct_stubs.a %s/../ocaml/libbigarray.a  -ldl -lns3.15-core -lns3.15-network -lns3.15-point-to-point -lns3.15-mpi -o %s.native" module_name lib lib lib lib lib module_name;
+    return ()
+  end else
+    Util.error "xen object file %s not found, cannot continue" obj
+
+(*  let _ = Unix.system (sprintf "ocamlbuild topo_%s.nobj.o" module_name) in 
   let _ = Unix.system (sprintf "mir-build -b ns3-native -o %s.native _build/topo_%s.nobj.o" 
                          module_name module_name) in 
   let _ = Unix.system (sprintf "rm topo_%s.ml" module_name) in 
-  return ()
+  return () *)
 
-let run_scenario sc = 
+let run_scenario sc _ = 
   let _ = Unix.system (sprintf "mpirun -n %d ./%s.native" 
                          (get_scenario_node_count sc) (get_scenario_name sc)) in 
   return ()
